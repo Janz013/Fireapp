5f3117fd3d29c06d6cdcc006a3829d343337992f
john mark ca¤ares <unknown7315@gmail.com>
2025-04-15 13:13:09 +0800

    updated it with the code

--- projectsite/fire/views.py
+++ projectsite/fire/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from fire.models import Locations, Incident
+from fire.models import Locations, Incident, FireStation
 
+def map_station(request):
+    # Fetch fire station data with name, latitude, and longitude
+    fireStations = FireStation.objects.values('name', 'latitude', 'longitude')
+
+    # Convert latitude and longitude to float for each fire station
+    for fs in fireStations:
+        fs['latitude'] = float(fs['latitude'])
+        fs['longitude'] = float(fs['longitude'])
+
+    # Convert QuerySet to a list
+    fireStations_list = list(fireStations)
+
+    # Context to pass to the template
+    context = {
+        'fireStations': fireStations_list,
+    }
+
+    # Render the template with context
+    return render(request, 'map_station.html', context)
 
 class HomePageView(ListView):
     model = Locations
